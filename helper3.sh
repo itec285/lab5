0a023b95eb27c25ee9017750e5752e044c1db70b
itec285 <i.am.mikelong@gmail.com>
2017-01-29 14:20:37 -0700

    Add comments and comment everything out.

--- helper3.sh
+++ helper3.sh
@@ -1,13 +1,11 @@
+# Windows Server 2012/2016 AD Password Cracker script Part III.
+# Jan 2017, M. Long
+#
+# This script automates the process of installing several necessary tools into Kali Linux 2016.2012/2016
+# Credit for most of this to: http://hackers-workshop.net/how-to-install-smbexec2-on-kali-linux
+
+#This section is used to mount the VM's drive, copy the ntds.dit database, the system key, and then unmount the drive.
 #mount /dev/sda2 /mnt
 #cp /mnt/Windows/NTDS/ntds.dit ~
 #cp /mnt/Windows/System32/config/SYSTEM ~
-
-#Start extracting the ntds.dit file to exportable files
-echo \######################################################################
-echo \#  Starting Initial Table Extraction.  This may take a few minutes.  #
-echo \#                                                                    #
-echo \######################################################################
-cd /opt/libesedb/esedbtools
-./esedbexport -m tables /root/ntds.dit
-echo Moving files to root directory
-mv ntds.dit.export/ ~
+#umount /mnt
